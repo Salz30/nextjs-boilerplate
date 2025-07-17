5c12144fa97b2525f5046104668ae0c8fffc15ec
Salz30 <158138908+Salz30@users.noreply.github.com>
2025-07-17 22:26:25 +0700

    Add .next/static/chunks/src_app_submit_page_tsx_0ef0cac3._.js 1752765982089

---
+++ .next/static/chunks/src_app_submit_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_dab103dd._.js",
+  "static/chunks/node_modules_f062c279._.js"
+],
+    source: "dynamic"
+});
